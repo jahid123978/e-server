fd131b7c553f7b79966dd961efed82a7441085e3
jahid123978 <jahidhasan21cse@gmail.com>
2025-05-15 23:04:09 +0600

    e-server

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
